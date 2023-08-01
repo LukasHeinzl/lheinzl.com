5519ed27e8609de35c2e3553ce88bccb6422f44c
lukasheinzl <l.heinzl.stp@gmail.com>
2023-08-01 20:18:05 +0200

    added blog post

---
+++ src/routes/blog/2023-08-01-mental18/+page.svelte
@@ -0,0 +1,195 @@
+<script>
+    import Post from "$lib/Post.svelte";
+
+    // from +layout.ts
+    export let data;
+</script>
+
+<Post data={data.postData} showPostList>
+    <p>
+        It's been a while...
+        Three months to be exact.
+        And a <em>lot</em> has happened in these three months.
+        This post serves as a small summary of what I've been up to, as well as a small update on the future of this
+        blog.
+        It'll be split into three parts and we're starting it off with something positive.
+    </p>
+
+    <p>
+        I successfully graduated from university and can now call myself a <strong>Bachelor of Science</strong>.
+        Initially, I was quite worried about my thesis and the exam itself but in the end it all went well.
+        Why was I worried?
+        You'll find out in the next part of the post.
+        For now though, we'll stick to some more positive news.
+        While writing this I'm starting my full-employment at my university.
+        I'll continue working in the Lab Services team, which I am very thrilled about.
+        Also, my dream come true: I will be teaching software development in the upcoming semester.
+    </p>
+
+    <p>
+        On another note:
+        July has been quite a pleasant month for a change.
+        After finishing my studies I had a month of downtime before starting to work.
+        I used this time to relax and do some things I've been wanting to do for a while.
+        For example:
+        I started playing more games again and one that I've been looking forward to released mid-july.
+        If you want to experience a cool new mechanic in a puzzle game, then I highly recommend to check out
+        <a href="https://store.steampowered.com/app/1382070/Viewfinder/">Viewfinder</a>.
+        Although I think that it was a bit short and the story a bit of an add-on, I still really enjoyed it!
+        Furthermore, I reworked my website a bit internally.
+        You can now click the tags in the post list to filter for posts with that tag.
+        I also tweaked the tags a bit so that the filter-system actually is useful.
+        And, of course, I added my shiny new title to my name on the landing page <strong>&gt;:)</strong>
+        Lastly, I've been on holiday, but more on that at the end of the post.
+    </p>
+
+    <p>
+        Now, let's get to the not so positive part.
+        There have been some major, let's say, "happenings" in my life.
+        Contrary to most other posts on here, what I'm writing about isn't in the past but still ongoing.
+        Therefore, I won't go that much into detail, since I have no clear view on the situation myself.
+        Also, I'm not on speaking terms with the people involved and it would feel wrong to write about them in detail.
+        But, to the best of my ability, I'll try to summarize what happened, since this helps me to sort things out in
+        my head.
+    </p>
+
+    <p>
+        As hinted towards in my last post <a href="/blog/2023-05-04-mental17/">17. The birds and the bees</a> I've
+        been in the closest friendship I've ever had.
+        And, I've developed feelings for someone.
+        All the details will, someday, be revealed in a future post.
+        For now, I'll just say that things haven't worked out, in multiple ways.
+        Needless to say, not only did the relationship not work out, I've also lost my closest friend.
+        It's been a very difficult time since the end of May but things have thankfully gotten a lot better.
+        While this alone seems rough, there's actually a catch.
+        For the first time in my life, someone else has stated that they have feelings for me.
+        What started out as a very close friendship slowly developed into more.
+        However, there sort of was a mutual denial of the situation and we just kept telling each other that what we are
+        doing is just platonic.
+        Eventually, we actually stated our mutual feelings and even kissed.
+        So, I guess at least I can now say that I experience my first kiss.
+        What followed, however, was not so pleasant...
+    </p>
+
+    <p>
+        Through details that I won't go into right now, things didn't work out.
+        After some days after the kiss I've made a very difficult decision: I've ended the friendship.
+        We were forced to see each other for a few more days until the month was over due to other reasons.
+        That wasn't easy.
+        I was extremely hurt and in pain and completely overwhelmed by my emotions.
+        In this state, I've made a lot of mistakes and stupid decisions.
+        I've hurt people that I care about and I've hurt myself.
+        The latter not only emotionally but also physically, for the first time ever.
+        Not my proudest moment.
+        I haven't done it since and I don't plan to do it again.
+        It's not worth harming yourself - I've learned that the hard way.
+    </p>
+
+    <p>
+        There have been a couple more back and forths with the person in question.
+        In a way of trying to get closure, or trying to get back the close friendship we had, I just made things worse.
+        It's kinda like burning down a bridge and then throwing dynamite down the chasm, just to be extra sure that it
+        won't get rebuilt.
+        Right now, I have no idea what they are thinking about me.
+        For all I know, they might as well just be done with me for good and hate me.
+        Which is a very sad thought, since I do believe that we had a special friendship, even though things did not
+        work out.
+        There were some things that needed improvement in our friendship, but I think that we could have worked on them.
+        I know (or at least strongly assume) that no-one involved acted in bad faith.
+        And I know that it was (and still is) an extremely difficult situation for everyone involved.
+        Yet, it is still difficult to get over the pain and forgive them.
+    </p>
+
+    <p>
+        That's something I struggle with in general, though.
+        Even the smallest argument, where we part ways in a bad mood, is something that I have a hard time getting over.
+        The next time I'd see that person they would be fine again while my brain is still stuck on the negative.
+        I'm always wondering if they are still mad at me or hate me.
+        In regard to the current situation, I just wish that I could have handled it better.
+        But, as always, I cannot change the past, only learn from it.
+        There hasn't been a single day that I haven't thought about them or what happened.
+        I miss them - or at least I think so.
+        I am unsure if miss them or just the idea of having someone this close.
+        Again, I do think that we had a special friendship and connected quite deeply and had a lot of understanding for
+        each other.
+        But right now there are too many open questions and no answers in sight.
+        It needs more time, that's for sure.
+        Maybe having a small talk would help, but I am not too sure about that as it could just make things worse.
+        Or I could get my answers of not being welcomed at all anymore.
+        But I have to look on the bright side and just make the best of the situation.
+        It did spark the idea for two new songs as well, so there is always something positive to look at.
+        Which leads me to the last part of this post.
+        Again, more details and a more in-depth analysis of the situation will follow in a future post, once I've had
+        more time to process everything.
+        And, once the people in question would be ok with me writing this, since some of the details are <em>very</em>
+        personal.
+    </p>
+
+    <p>
+        So, what have I learned from this?
+        Well, quite a lot actually.
+        On the surface it might not seem that much and I've also felt like making no progress at all.
+        But I've done quite a lot of self-reflection and learning in the past two months.
+        There is a thing called <em>Cognitive Behavioral Therapy (CBT)</em> which, in simple terms, focuses on how
+        thoughts, feelings and behavior are connected.
+        It sort of takes a scientific approach to psychology.
+        And science and logic are right up my alley.
+        I've been reading a lot about CBT and I've been trying to apply it to my life.
+        Its main thing is called "cognitive distortions".
+        Basically, it's a list of common thinking errors that people make.
+        The goal is to identify these errors and then try to correct/challenge them.
+
+        In a nutshell, you always have different ways of thinking about a situation.
+        And thinking about it in a negative way will make you feel bad.
+        So, if you can identify that you are thinking in a negative way, you can try to challenge that thought and
+        think about it in a more positive way.
+        This will then make you feel better.
+        And, if you feel better, you will behave better.
+        Sounds simple and straightforward, but it isn't.
+        It takes a lot of practice and it's not easy to identify your own thinking errors.
+        But after a while you get better at it and it really helps.
+    </p>
+
+    <p>
+        Now, let's round this post off.
+        As stated in the beginning, I've been on holiday.
+        Well, before that, I actually got sick for a week which wasn't the best experience.
+        The holiday, however, was great.
+        It was a gift from my parents for graduating from university.
+        We spent five days, mostly in Switzerland.
+        This, in fact, leads me to the future of this blog.
+        Right now, it's only been about my mental struggles, mostly in chronological order.
+        I've been thinking about writing about other things as well.
+        So, here's the plan:
+        The first tag of each post will indicate the category.
+        All posts so far have been tagged with "Mental health".
+        More general posts will be tagged with "General".
+        And, posts about my travels will be tagged with "Travel".
+        In the future, more categories might be added as needed.
+
+        So, the next post will be about my holiday.
+        As for the mental health stuff.
+        Well...
+        Since the main story came to a grinding halt at November last year, and the people that are involved in the
+        current story are not on speaking terms with me, there's not much to write about.
+        Well, there is <em>a lot</em> to write about, but I don't feel comfortable writing about it right now.
+        There are more things that I do want to write about, including an already laid out post of the intermission
+        flavour, as well as more content about CBT.
+        So stay tuned for that.
+    </p>
+
+    <p>
+        On that note, I'll end this post.
+        Thanks for reading, as always.
+        I want to do something fun at the end, which is a bit of a throwback to some of the first posts:
+        A little bit of homework for you.
+        Go ahead and reach out to someone who you hold dear and let them know that you care about them.
+        It doesn't have to be anything big, just a simple "Hey, I just wanted to let you know that I care about you."
+        Because you never know when you might lose them and close people are important.
+        Also, on a similar note: <a href="https://www.youtube.com/watch?v=F4kcczygOcw">Song recommendation!</a>
+        Lastly, special thanks to A and S (male, first introduction) for being there for me.
+        I have probably strained you more than you like to admit and I'm sorry for that.
+        But I will be forever grateful for your support.
+        And now, for real, bye bye!
+    </p>
+</Post>
