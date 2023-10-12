15e8bf632610e6b337eacf73521f83e59cf6a7d1
lukasheinzl <l.heinzl.stp@gmail.com>
2023-10-12 22:17:50 +0200

    Added blog post

---
+++ src/routes/blog/2023-10-12-general2/+page.svelte
@@ -0,0 +1,83 @@
+<script>
+    import Post from "$lib/Post.svelte";
+
+    // from +layout.ts
+    export let data;
+</script>
+
+<Post data={data.postData}>
+    <p>
+        As by the spirit of the new "general" category, here is another (hopefully shorter) post about the recent
+        happenings.
+        My plan, in <i>general</i> (get it??) is to write shorter posts in the general category whenever enough
+        interesting things have happened recently, while longer, more in-depth, mental health posts will follow in the
+        future, covering longer time periods.
+        And since its been a bit over two months now and things definitely happened, here we go.
+    </p>
+
+    <p>
+        You may remember that last time I shed some light on my, let's say, questionable experiences with love.
+        Well, I'm happy to report that things have changed for the better.
+        No, I'm not in a relationship, don't get your hopes up, assuming you were rooting for me.
+        The main person mentioned in the last general post and I actually had a talk.
+        Well, we need to backtrack a bit actually.
+        First of all, I made the spontaneous decision to buy tickets for a festival in my home town.
+        If been there last year for one of the three days and it was incredible (<a
+            href="/blog/2023-04-23-mental16">16. Things are looking up</a>).
+        So, even though there weren't any artists that I absolutely had to see, there were at least a few that I was
+        interested in.
+        Long story short: Best decision to go.
+        On the first day, in the early afternoon, I got a text from the person mentioned previously.
+        In short, it was more of a heated / emotional message and the details don't matter right now, but we ended up
+        meeting the monday after the festival.
+    </p>
+
+    <p>
+        So, it's monday morning and I am still in the festival high.
+        At first I felt a bit nervous, but overall I was just in too much of a happy "everything is a-ok" mood.
+        We talked for about an hour and basically came to the conclusion that we were both interested in building up our
+        friendship again.
+        Yes, things had happened in the past and they were <i>far</i> from great, but we both agreed that we've learned
+        from that and that there is a way to move forward.
+        Over the next few days and weeks we talked a lot more about how we felt and also what boundaries we had.
+        Overall, the importance of open communication was stressed a lot and we have been doing an okay job at it.
+        I do believe that there is room for improvement, and I am referring quite a lot to myself here, but not
+        exclusively.
+        We will see how things go, only time will tell.
+    </p>
+
+    <p>
+        Starting my masters degree has been really great so far.
+        However, the first two days were quite overwhelming because there were many new people.
+        And, despite the fact that <i>nameless person from above</i> and I get along again, it was strange and a bit
+        awkward and uncomfortable to see the old group back together again.
+        Because of a lot of stress at work, I actually had a rough time again briefly and even a small breakdown where
+        I came home in the evening and just went sobbing into my pillow.
+        Not a great time...
+    </p>
+
+    <p>
+        Last part, ending on something positive >:)
+        I've known someone for about a year now, because we sort of work together, but we haven't really talked much
+        until now.
+        So, I randomly decided to text them and ask if they wanted to hang out.
+        After a few, at least for me, awkward texts back and forth we agreed to meet up.
+        And now I can say that I have extended my very small circle of friends by one awesome person.
+        We've met up a few times now and it's been really great.
+        Now, really onto the last thing to say.
+        At the time of writing this, last friday (October 6th) was a great day in the world of music.
+        Two of my favorite artists released new albums / songs and I am so happy about it.
+        I've been listening to them non-stop and I am not even close to being tired of them.
+        First: <a href="https://www.youtube.com/watch?v=Ck9_Fx5DSz4">Normal People Things by Lovejoy (Single,
+        Youtube)</a>.
+        And second: <a href="https://open.spotify.com/album/4KAtLRVIfB0bKnRY01dveY">
+        Lucid Dreams by BoyWithUke (Album, Spotify)
+    </a>.
+        My personal shout-out and recommendation at the very end:
+        <a href="https://www.youtube.com/watch?v=giisqo8JZQ8">Bad Things</a> really stood out to me.
+        It's basically about the singer being in a bad mental state in general, but having someone close who comforts
+        them.
+        That really speaks to me, even-though I do not have that someone on my side.
+        Anyway, go listen to these amazing songs and have a nice time :)
+    </p>
+</Post>
