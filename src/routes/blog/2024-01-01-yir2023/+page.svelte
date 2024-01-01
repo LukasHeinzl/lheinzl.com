a9aed011da94c184ba82a2f2365b2d7aafcd1eaf
lukasheinzl <l.heinzl.stp@gmail.com>
2024-01-01 21:57:41 +0100

    Added blog post

---
+++ src/routes/blog/2024-01-01-yir2023/+page.svelte
@@ -0,0 +1,103 @@
+<script>
+    import Post from "$lib/Post.svelte";
+
+    // from +layout.ts
+    export let data;
+</script>
+
+<Post data={data.postData}>
+    <p>
+        New year, old me.
+        Yeah, what did you think would happen?
+        Ain't nothing gonna change just because the year did.
+        New years is just a regular day for me.
+        But since it does mark an extremely important change for almost everyone else
+        (yes, a lot of people consider writing down the wrong year for a few weeks an important change),
+        I thought "heck, why not, let's do a short review of last year".
+        And so here we are.
+    </p>
+
+    <p>
+        I'm not gonna make this long and go into detail.
+        You can go through the posts yourself.
+        Now, they all have a 2023 tag, so in the future, you can easily filter by year.
+        Aren't I nice?
+        The very very <i>very</i> brief summary of 2023: A shit ton happened.
+        Normally, you'd pick out your favourite memory / moment and talk a bit about it.
+        For me, it funnily has always been easier to pick out the most negative memory / moment.
+        And so, if you want to read about the worst part of my 2023, then go ahead:
+        <a href="/blog/2023-08-01-general1/">Where I've been</a>
+    </p>
+
+    <p>
+        Anyway.
+        I thought a bit about what my favourite memory / moment of 2023 could be.
+        And a few came to mind and I am always bad at picking <i>the</i> best thing.
+        One highlight though was my Switzerland trip, especially being on top of the mountains at the Jungfraujoch.
+        I even printed out one of the panorama photos I took and hung it up at home.
+        So, if you want a brief overview of that holiday, then <a href="/blog/2023-08-13-travel1/">Trip to
+        Switzerland</a> has got you covered.
+        Otherwise, there were loads and loads of fun moments with friends and colleagues.
+        And, of course, I cannot forget to stress how much I am grateful for the few, but extremely close, friends I
+        have had the honor of spending loads of time with in 2023.
+        If you read this, you know who you are.
+        Love you!
+    </p>
+
+    <p>
+        Now onto a finisher of the year, since I haven't written in a few months.
+        Not that much has been going on actually.
+        I was sick twice, with a week's break in between.
+        Cannot recommend.
+        Otherwise, December has been worse again with my mental health.
+        It probably has something to do with winter, because I spend most of my time inside.
+        Oh precious sun, where are you.
+        Christmas time was as usual, but I got a lot of Lego (mostly from myself, because I wanted bigger, more
+        expensive sets) and oh my god did I miss building Lego.
+        Another gift to myself: I am moving out for the first time.
+        I'm not on track to my own apartment just yet, but I am moving into a shared flat at my university's students
+        dorm, where one of the rooms will be free soon.
+        And then I'll be living with a friend of mine.
+        It is a bit scary to actually move out from my parents' home, but I am <i>very</i> excited.
+    </p>
+
+    <p>
+        Lastly, I do want to document my very sad finish of the year.
+        December 31st was not a great day.
+        I was feeling down and I was alone at night.
+        My parents were out and about.
+        Then, suddenly it kicked in quite fast.
+        My mind was full of negative thoughts and I also felt a bit angry at everything.
+        I had a lot of bad thoughts about my friends and if they even care about me and like me.
+        I thought if it was even worth my time to be friends with them.
+        And so on, and so forth.
+        I was in a very bad place.
+        I was crying a lot.
+        And, for a brief moment, I was thinking about self-harm again.
+        But, I was able to gather myself at least a little bit and reached out to someone.
+        I expected that they aren't going to respond until the next day, but it felt good to let off some steam.
+        I then cried myself to sleep.
+        A bit after midnight, I woke up and felt very sick.
+        I was on the brink of throwing up.
+        After some time, it got a bit better and I fell asleep again.
+        And that concludes my 2023, as it is now already 2024 in my story.
+    </p>
+
+    <p>
+        So, what's next?
+        First of all, one more week of winter break.
+        Then it's back to university and grinding on all projects to finish them on time until the end of the month.
+        As for the bigger picture.
+        I do not have anything concrete planned for the year yet.
+        My overall goal is just to be a better version of myself, day by day.
+        I want to be more open about my feelings and thoughts.
+        I want to be more honest with myself and others.
+        I want to be more confident in myself.
+        And so on.
+        Will I achieve all of that?
+        God knows.
+        But I will try.
+        And I hope you will too.
+        Have a great 2024!
+    </p>
+</Post>
