4d1b77588bbf26fa5931765c1081109aa3480f5e
Lukas Heinzl, BSc <l.heinzl.stp@gmail.com>
2024-07-17 20:40:25 +0200

    Fixed 404s by prerendering everything

--- svelte.config.js
+++ svelte.config.js
@@ -13,10 +13,7 @@ const config = {
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter({
             fallback: '404.html'
-        }),
-        prerender: {
-            entries: []
-        }
+        })
     }
 };
 
